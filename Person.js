fdbeebef3c9ff5d586d7ba37a685d9c9197f8610
Komal-8080 <shindeg163@gmail.com>
2021-02-11 23:36:50 +0530

    ADDED: Address Book With Person Constacts Created

---
+++ Person.js
@@ -0,0 +1,20 @@
+class Person{
+
+    constructor(...params){
+        this.firstName=params[0];
+        this.lastName=params[1];
+        this.address=params[2];
+        this.city=params[3];
+        this.state=params[4];
+        this.zip=params[5];
+        this.phoneNumber=params[6];
+        this.email=params[7];
+    }    
+
+    toString(){
+        return '\nFirstName: ' + this.firstName + ' LastName: ' + this.lastName + ' Address: ' + this.address+ ' City: '+ this.city+' State : '+ this.state+ ' Zip: '+this.zip+' PhoneNumber: '+this.phoneNumber+' Email: '+ this.email;
+    }
+}
+
+let personData = new Person("Komal","Shinde","Sion","Mumbai","Maharastra",400017,'91 7712034524','xyz@yahoo.com');
+console.log(personData.toString());
